6f7bb9142192e82717c34dd149f5da64411f1b6f
kianasaraki <111961546+kianasaraki@users.noreply.github.com>
2022-11-18 12:33:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'omocL605xJBwPYM8IE1jXPsk0edc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mr__e_r063dLaXkAf0HnanymMF7Vsk-4ACG3p73lPkk',
+      useTemplateId: '	mr__e_r063dLaXkAf0HnanymMF7Vsk-4ACG3p73lPkk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
